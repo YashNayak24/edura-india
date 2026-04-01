6aaf13014370a01e481d88256acd1a3db9adc889
yash <ynayak658@gmail.com>
2026-04-01 12:23:38 +0530

    fix cors path

--- backend/server.js
+++ backend/server.js
@@ -15,7 +15,8 @@ const PORT = process.env.PORT || 5000;
 app.use(cors({ origin: [
     "http://localhost:5173",
     "https://edura-india.vercel.app",
-    "https://www.eduraindia.com"
+    "https://www.eduraindia.com",
+    "https://eduraindia.com"
   ],
   credentials: true }));
 app.use(express.json());
